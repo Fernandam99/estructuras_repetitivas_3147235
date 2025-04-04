878bff65143e0f4c0d41c0e79a32011cf9167ba3
Kevin <kevinfonseca807@gmail.com>
2025-04-04 11:32:11 -0500

    clase

--- ciclo_while.py
+++ ciclo_while.py
@@ -7,7 +7,7 @@ numero = int(input("Ingrese un numero: "))
 
 i=10
 
-while i>=10:
+while i<=10:
     resultado = numero* i
     print(numero, "x" , i, " = ", resultado)
     ##instruccion de incremento
